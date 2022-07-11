63bc905f13caecc24ae56fecbb0c87c4fd28b638
Vinit Dhull <a7793@ind.aptaracorp.com>
2022-07-11 14:42:37 +0530

    ADDED TINYMCE RICH TEXT EDITOR IN INDEX VIEW BY VINIT

--- bioRefProject/bioRefApp/views.py
+++ bioRefProject/bioRefApp/views.py
@@ -15,6 +15,7 @@ from io import BytesIO
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .ai_model_output import biorefOutput
+import re
 
 # Create your views here.
 @login_required(login_url='login')
@@ -31,14 +32,16 @@ def index(request):
         return render(request, "index.html", context)
     elif request.method == "POST" and request.POST.get("form_type") == "formTwo":
         inpt = request.POST.get("input_text")
+        inpt = re.sub('<[^<]+?>', '', inpt)
         otpt = request.POST.get("output_text")
+        otpt = re.sub('<[^<]+?>', '', otpt)
         time_stamp = str(datetime.datetime.now().strftime("%Y-%m-%d--|--%H:%M"))
         data_obj = InputInfo(user_name=request.user, time_of_process=time_stamp, input_data=inpt, output_data=otpt)
         try:
             data_obj.save()
             context = {
                 "item": otpt,
-                "data": data
+                "data": inpt
             }
             messages.success(request, "Your data saved successfully!")
         except:
